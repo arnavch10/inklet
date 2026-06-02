4b57dd9edb5bfa51c4e727dbd087427e41bb539d
hipika <arnavchoudhary53@gmail.com>
2026-06-01 21:17:47 -0400

    add navbar and landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,9 @@
 export default function LandingPage() {
 
     return (
-        <>
+        <main className="pt-32">
             <h1>this is the landing page</h1>
-        </>
+        </main>
 
     );
 }
--- components/landing-page/Navbar.tsx
+++ components/landing-page/Navbar.tsx
@@ -9,12 +9,31 @@ export default function Navbar() {
   return (
 
       <nav className="fixed top-10 left-0 w-full flex justify-center">
-        <div className="flex items-center justify-between gap-6 rounded-full bg-white/70 backdrop-blur-md shadow-lg shadow-black border-2 px-6 py-3">
-          <p className={`${space.className} font-bold text-xl`}>Inklet</p>
+        <div className="flex items-center justify-between gap-6 rounded-full bg-white/70 backdrop-blur-md shadow-lg shadow-black/40 border border-black/10 px-6 py-3">
+          <Link className={`${space.className} font-bold text-xl`} href="/">Inklet</Link>
 
-          <div className="rounded-full bg-gray-700 px-4 py-1">
+          {/* Make one div for the buttons later*/}
+
+          <div className="rounded-full bg-gray-700 px-4 py-1 hover:bg-blue-300 hover:scale-110 transition-all duration-300 ease-in-out">
+            <Link className={`${space.className} font-medium text-sm text-white `} href="/about">About</Link>
+          </div>
+
+
+
+          <div className="rounded-full bg-gray-700 px-4 py-1 hover:bg-blue-300 hover:scale-110 transition-all duration-300 ease-in-out">
             <Link className={`${space.className} font-medium text-sm text-white`} href="/login">Login</Link>
           </div>
+
+          
+          <div className="rounded-full bg-gray-700 px-4 py-1 hover:bg-blue-300 hover:scale-110 transition-all duration-300 ease-in-out">
+            <Link className={`${space.className} font-medium text-sm text-white `} href="/signup">Sign Up</Link>
+          </div>
+
+          <div className="rounded-full bg-gray-700 px-4 py-1 hover:bg-blue-300 hover:scale-110 transition-all duration-300 ease-in-out">
+            <Link className={`${space.className} font-medium text-sm text-white `} href="/canvas">Canvas</Link>
+          </div>
+          
+
 
         </div>
 
